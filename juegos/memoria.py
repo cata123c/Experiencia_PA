b2b5738ac9e225975057f22fd82d0d5bb57859ea
hovyman <163353481+hovyman@users.noreply.github.com>
2024-03-14 20:20:15 +0000

    Memoria.py final

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -5,13 +5,13 @@ import os
 lista_num=[]
 a=0
 def memoria():
-    for i in range(4):
+    for i in range(6):
         lista_num.append(random.randint(1,9))
     print("Recuerda la siguiente sequencia de numeros:")
     print(lista_num)
     time.sleep(6)
     os.system("clear")
-    print("Inserta los numeros de esta manera [1, 2, 3, 4, 5, 6, 7, 8]")
+    print("Inserta los numeros de esta manera [1, 2, 3, 4, 5, 6]")
     a=input("¿Cual era la secuencia de numeros?: ")
     if a==str(lista_num):
         print("Correcto, tienes buena memoria")
