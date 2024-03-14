a735552c01420d40d6f6e0c1847866a379ef58cd
BenjaaVega <163360234+BenjaaVega@users.noreply.github.com>
2024-03-14 19:14:57 +0000

    Juego del dado terminado

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -1,8 +1,37 @@
+import random
+
 def juego_del_dado():
-    """
-    Esta función tiene que pedirle al usuario que aprete enter para que lance un dado.
-    Esto genera un número al azar que se le suma a la puntuación del usuario.
-    Después el computador también tiene que lanzar un dado.
-    El primero en sumar 30 puntos gana.
-    """
-    pass
+    jug1 = 0
+    jug2 = 0
+    sepuede = True
+    while sepuede:
+        print("Apreta Enter para lanzar el dado")
+        input()
+        lanzamiento = random.randint(1,6)
+        print("El número obtenido fue "+str(lanzamiento))
+        jug1 += lanzamiento
+        if jug1 >= 30:
+            ganador = 1
+            sepuede = False
+        elif jug1 < 30:
+            print("Llevas "+str(jug1)+" puntos")
+            lanzamiento2 = random.randint(1,6)
+            print("Tu adversario saco "+str(lanzamiento2))
+            jug2 += lanzamiento2
+            if jug2 >= 30:
+                ganador = 2
+                sepuede = False
+            elif jug2 < 30:
+                print("Tu adversario lleva "+str(jug2)+" puntos")
+    if ganador == 1:
+        return "Has llegado primero a 30 puntos!!"
+    elif ganador == 2:
+        return "Tu adversario llego primero a 30 puntos"
+
+         
+
+
+
+
+n = juego_del_dado()
+print(n)
